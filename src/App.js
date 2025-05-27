07421b0d7693ec614504f60fe1218933d0e0cc71
shrenik2703 <102521670+shrenik2703@users.noreply.github.com>
2025-05-27 20:51:03 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
 
   // Function to handle sending a message to the iframe
   const handleSendCommand = () => {
-    const message = { message: 'test' };
+    const message = { console: 'r.setres 1000x2000w' };
 
     if (iframeRef.current) {
       iframeRef.current.contentWindow.postMessage(message, '*');
